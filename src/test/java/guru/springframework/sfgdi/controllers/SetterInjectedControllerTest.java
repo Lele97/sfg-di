0653948470f33df89e57d20a629700be5409a76b
Lele97 <gabriele.grandinetti@hotmail.com>
2023-08-08 10:24:17 +0200

    commit Dependency Inversion Principle exemple

--- src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.GreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +12,7 @@ class SetterInjectedControllerTest {
     @BeforeEach
     void setUp() {
         controller = new SetterInjectedController();
-        controller.setGreetingService(new ConstructorGreetingService());
+        controller.setGreetingService((GreetingService) new ConstructorGreetingService());
     }
 
     @Test
