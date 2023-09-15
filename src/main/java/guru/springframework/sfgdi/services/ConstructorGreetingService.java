94f3e1fce78441d0fd23b771b75f576104b898df
Lele97 <gabriele.grandinetti@hotmail.comcd>
2023-09-15 16:54:34 +0200

    Add Configuration class with annotation  @Configuration remove @Service annotation in classes: ConstructorGreetingService.java, SetterInjectedGreetingService.java, PropertyInjectedGreetingService.java and add relative @Bean  in a Configuation class

--- src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
@@ -2,8 +2,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class ConstructorGreetingService implements GreetingServices{
+public class ConstructorGreetingService implements GreetingServices {
 
     @Override
     public String sayGreeting() {
--- src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -2,8 +2,9 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class PropertyInjectedGreetingService implements GreetingServices{
+
     @Override
     public String sayGreeting() {
         return "Hello World - Property";
